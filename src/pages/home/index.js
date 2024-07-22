08939feed994695a08adb637ac9ba632f393801b
서원빈 <1bins@kakao.com>
2024-07-22 11:22:09 +0900

    :seedling: 인트로 페이지 생성

---
+++ src/pages/home/index.js
@@ -0,0 +1,17 @@
+import Intro from "../intro";
+
+const Home = () => {
+  return(
+      <>
+        <Intro/>
+        <section id="home">
+          <div className="home-inner">
+
+          </div>
+        </section>
+      </>
+
+  )
+}
+
+export default Home;
---
+++ src/pages/intro/index.js
@@ -0,0 +1,11 @@
+const Intro = () => {
+  return(
+      <section id="intro">
+        <div className="intro-inner">
+          intro
+        </div>
+      </section>
+  )
+}
+
+export default Intro;
