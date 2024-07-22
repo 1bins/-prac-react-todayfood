2a8f0f897f9698865e139103e6742bd1c9ab7899
서원빈 <1bins@kakao.com>
2024-07-22 09:50:11 +0900

    :sparkles: 강수량에 따른 class('--rainy') 부여

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,9 +1,31 @@
 import {Outlet} from "react-router-dom";
+import {useEffect, useState} from "react";
+import classNames from "classnames/bind";
+import {WeatherModule} from "../../utils";
 
 const Layout = () => {
+  const [temp, setTemp] = useState(0);
+  const [isRainy, setIsRainy] = useState(false);
+  const setWeather = (data) => {
+    data.forEach(item => {
+      if (item.category === 'T1H') setTemp(item.obsrValue);
+      if (item.category === 'PTY') setIsRainy(Number(item.obsrValue) !== 0);
+    })
+  }
+
+  useEffect(() => {
+    WeatherModule()
+        .then(response => {
+          setWeather(response)
+        })
+        .catch(response => {
+          setWeather(response)
+        })
+  }, []);
+
   return(
       <main>
-        <div className="main">
+        <div className={classNames('main', {'--rainy': isRainy})}>
           <Outlet />
         </div>
       </main>
